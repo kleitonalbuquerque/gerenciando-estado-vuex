4b65fed9615e12e2174773cb914d414157fba245
kleitonalbuquerque <kleiton2102@gmail.com>
2021-03-10 11:51:54 -0300

    Gerenciando estado com vuex

--- src/vuex.js
+++ src/vuex.js
@@ -9,7 +9,7 @@ const store = new Vuex.Store({
 	},
 	mutations: { // mutations são responsáveis por mudar o valor da state(estado)
 		decrement: state => state.counter--,
-		increment: (state) => state.counter++
+		increment: state => state.counter++
 		// increment: (state, num) => state.counter =  num * 2
 		// increment: state => state.counter = state.counter + 10
 	},
